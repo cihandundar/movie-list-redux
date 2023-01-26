9ffe65e7a1f5f44109cd154755974f16d8264c14
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-01-27 00:22:00 +0300

    router layout set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "pages";
 function App() {
-  return <div className="App">deneme</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
