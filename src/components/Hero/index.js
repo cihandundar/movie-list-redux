7d92d6ad8c49c0745141f2a027e8e250420013ab
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-01-27 23:37:46 +0300

    hero component added

---
+++ src/components/Hero/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Movies from "assets/images/movies-1.png";
+import { Link } from "react-router-dom";
+const Hero = () => {
+  return (
+    <section>
+      <div className="hero">
+        <div className="hero__container">
+          <div className="hero__image">
+            <img src={Movies} alt="" />
+          </div>
+          <div className="hero__text">
+            <div className="hero__title">
+              <h1>HD-Movies</h1>
+            </div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
+              dolor a neque perferendis officiis nulla. Magni at quisquam, ex
+              laborum quia fugit corrupti expedita temporibus perspiciatis
+              dignissimos totam enim alias unde earum, itaque molestias aut in,
+              sequi eos. Cumque praesentium dolorem, totam natus eligendi saepe
+              est error, maiores rem nobis beatae illo perferendis assumenda
+              dolores excepturi fugiat deleniti consequuntur libero.
+            </p>
+            <button className="btn">
+              <Link to="/movies">See All</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
