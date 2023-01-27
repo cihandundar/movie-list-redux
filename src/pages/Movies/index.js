02a273a34c1299536fbd7f10f8e2ecd4c9020d92
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-01-27 23:39:41 +0300

    added loading spinner

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import { useState } from "react";
+import Loading from "assets/images/loading.gif";
+import { MovieItem } from "components";
 const Movies = () => {
-  return <div>Movies</div>;
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3500);
+  return (
+    <section className="container">
+      {loading ? (
+        <div className="container__loading">
+          <img src={Loading} alt="" />
+        </div>
+      ) : (
+        <MovieItem />
+      )}
+    </section>
+  );
 };
 
 export default Movies;
