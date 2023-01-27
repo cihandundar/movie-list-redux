29079061632b3d0778700d1186875cb41a1db894
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-01-27 13:03:35 +0300

    added navigation bar layout

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "assets/images/logo.png";
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "Movies",
+      path: "/Movies",
+    },
+    {
+      id: 3,
+      name: "Login / Register",
+      path: "/login",
+    },
+  ];
+
   return (
-    <nav>
-      <div className="logo">
-        <h1>HD-Movies</h1>
+    <nav className="nav">
+      <div className="nav__container">
+        <div className="nav__logo">
+          <img src={Logo} alt="" />
+          <h2>HD-Movies</h2>
+        </div>
+        <ul className="nav__list">
+          {links?.map((link) => (
+            <li key={link?.id} className="nav__list__link">
+              <Link to={link?.path}>{link?.name}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   );
