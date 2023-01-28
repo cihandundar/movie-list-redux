21eb6e808360086d7ca5a93095fa624c04875a96
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-01-28 16:35:14 +0300

    info component removed

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import Navbar from "./Navbar";
 import MovieItem from "./MovieItem";
 import TrendingMovies from "./TrendingMovies";
 import MovieSlider from "./TrendingItem";
-import Info from "./Info";
+
 import Footer from "./Footer";
-export { Navbar, MovieItem, TrendingMovies, MovieSlider, Info, Footer };
+export { Navbar, MovieItem, TrendingMovies, MovieSlider, Footer };
