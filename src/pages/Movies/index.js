54146d855a14ae12423303bbbea6f3ec5120d462
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-04-19 23:45:17 +0300

    loading removed

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { useState } from "react";
-import Loading from "assets/images/loading.gif";
 import { MovieItem } from "components";
 
 const Movies = () => {
-  const [loading, setLoading] = useState(true);
-  setTimeout(() => {
-    setLoading(false);
-  }, 3500);
   return (
     <section className="container">
-      {loading ? (
-        <div className="container__loading">
-          <img src={Loading} alt="" />
-        </div>
-      ) : (
-        <div className="container__movie">
-          <MovieItem />
-        </div>
-      )}
+      <div className="container__movie">
+        <MovieItem />
+      </div>
     </section>
   );
 };
