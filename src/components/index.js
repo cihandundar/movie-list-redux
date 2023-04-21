cc6d89b3c166dfc298609ebf831b5e309c49e888
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-04-21 12:00:09 +0300

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,5 @@ import Navbar from "./Navbar";
 import MovieItem from "./MovieItem";
 import TrendingMovies from "./TrendingMovies";
 import TrendingItem from "./TrendingItem";
-
-export { Navbar, MovieItem, TrendingMovies, TrendingItem };
+import Filter from "./Filter";
+export { Navbar, MovieItem, TrendingMovies, TrendingItem, Filter };
